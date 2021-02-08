bffe70a0d05f12075046478e72cf46a24f79d747
AnastasiaWei <61657226+kuznechek@users.noreply.github.com>
2021-02-08 16:00:19 +0300

    Add files via upload

--- Fine_circuit.py
+++ Fine_circuit.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 from classes import *
 
 pygame.init()
@@ -8,19 +9,24 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Виселица 1.2")
 clock = pygame.time.Clock()
 
-decorations = pygame.sprite.Group()
-gameobjects = pygame.sprite.Group()
+decorations_group = pygame.sprite.Group()
+gameobjects_group = pygame.sprite.Group()
+screamers_group = pygame.sprite.Group()
+
+screamer = False
 
 while True:
     clock.tick(FPS)
 
     #Спрайты декораций
-    title = draw_decs(1, WIDTH / 2, 100)
-    gameobjects.add(title)
+    #title = draw_decs(1, WIDTH / 2, 100)
+    #decorations.add(title)
+    scr_im = draw_decs(3, 640, 360, decs)
+    decorations_group.add(scr_im)
 
     #Cпрайты игровых объектов
     for letter in draw_alphabet():
-        gameobjects.add(letter)
+        gameobjects_group.add(letter)
 
     #Выбор слова
     words = get_content(0, None)
@@ -29,21 +35,37 @@ while True:
     print(word.full)
 
     while True:
-        #Написать слово
-        word.draw(decorations, a, b)
+        clock.tick(FPS)
+
+        #tryies = 10
 
         for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                break
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    for obj in gameobjects:
+                    for obj in gameobjects_group:
                         if obj.check_collisions(event.pos) == True and obj.name in word.undiscovered:
                             word.update(obj.name)
                             obj.kill_me()
                             break
+                        elif obj.check_collisions(event.pos) == True and obj.name not in word.full:
+                            screamers_group.add(draw_decs(random.randint(0,2), 640, 360, screamers))
+                            screamer = True
+
+        if screamer == True:
+            screamers_group.draw(screen)
+            screamer = False
+            pygame.display.flip()
+            for s in screamers_group:
+                s.kill()
+            time.sleep(1)
+        else:
             # Рендеринг
-            screen.fill(BLACK)
-            decorations.draw(screen)
-            gameobjects.draw(screen)
-            # После отрисовки всего, переворачиваем экран
+            decorations_group.draw(screen)
+            gameobjects_group.draw(screen)
+
+            # Написать слово
+            word.draw(decorations_group, a, b)
             pygame.display.flip()
     break
--- classes.py
+++ classes.py
@@ -7,7 +7,8 @@ game_folder = os.path.dirname(__file__)
 assets_folder = os.path.join(game_folder, 'Assets')
 
 files = ['done_dic.txt', 'rep.txt', 'die.txt']
-decorations = ['Dunder.png', 'Title.png', 'FineFin.png']
+decs = ['Dunder.png', 'Title.png', 'FineFin.png', 'Bg.png']
+screamers = ['Scream1.png', 'Scream2.png', 'Scream3.png']
 
 indexes = list()
 
@@ -18,9 +19,9 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (130, 155, 205, 255)
 
-WIDTH = 1040
+WIDTH = 1280
 HEIGHT = 720
-FPS = 30
+FPS = 15
 
 a = 100
 b = HEIGHT/2 - 100
@@ -42,7 +43,6 @@ class Rectangle(pygame.sprite.Sprite):
         return False
 
     def kill_me(self):
-        print('"' + self.name + '" WAS KILLED.')
         self.kill()
 
 class Word():
@@ -61,8 +61,11 @@ class Word():
         self.game = ''.join(progress)
 
     def draw(self, group, a, b):
-        for k in self.game:
-            block = draw_block(k, a, b)
+        for obj in group:
+            if obj.name in self.game and obj.name in self.full:
+                obj.kill_me()
+        for obj in self.game:
+            block = draw_block(obj, a, b)
             group.add(block)
             a += 70
 
@@ -72,21 +75,34 @@ def draw_block(letter, x, y):
     block = Rectangle(letter, image, x, y)
     return block
 
-def draw_decs(n, x, y):
-    image = pygame.image.load(os.path.join(assets_folder, decorations[n]))
+def draw_decs(n, x, y, fd):
+    image = pygame.image.load(os.path.join(assets_folder, fd[n]))
     block = Rectangle('dec',image, x, y)
     return block
 
 def draw_alphabet():
-    w0 = WIDTH / 10
-    w,k,h = w0, 80, HEIGHT / 1.7
-    x, y = 1040, 1051
-    alphabet = set()
-    for j in range(3):
-        for i in range(x, y):
-            alphabet.add(draw_block(chr(i), w, h))
+    w0 = WIDTH / 20
+    w, k, h = w0, 80, HEIGHT / 1.7
+    alphabet_full = get_full_alphabet()
+    alphabet, d = set(), 0
+    for j in range(2):
+        for i in range(11):
+            alphabet.add(draw_block(alphabet_full[i+d], w, h))
             w += k
-        w, x, y, h = w0, x+11, y+11, h+100
+        w, h, d = w0, h+100, d+11
+    for i in range(23, 33):
+        alphabet.add(draw_block(alphabet_full[i], w, h))
+        w += k
+    return alphabet
+
+def get_full_alphabet():
+    x, y = 1040, 1072
+    alphabet = list()
+    for i in range(x, x + 6):
+        alphabet.append(chr(i))
+    alphabet.append('Ё')
+    for i in range(x + 6, y):
+        alphabet.append(chr(i))
     return alphabet
 
 def get_content(index, code):
